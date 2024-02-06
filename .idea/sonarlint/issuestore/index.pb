69d76993a841211e7fd8ea6b8ddd56d40845127a
ggam-nyang <v44ads@naver.com>
2024-02-06 20:41:06 +0900

    asio echoserver

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+>
+CMakeLists.txt,9/a/9a2aa4db38d3115ed60da621e012c0efc0172aae
+8
+main.cpp,6/8/682a50d19e416195436708e48c7761923b8d6651
+T
+$boost_1_82_0/boost/lambda/lambda.hpp,1/9/19b735ba517cc786e0ccff9ae4928b55502c5622
+:
+
+client.cpp,e/9/e9f294f4e8adb7e0c852601c784c1042ffee1d14
+:
+
+server.cpp,d/f/dfdee4e5c0388afee9cccd5f492f7a517cd25885
+=
+.clang-format,0/1/01a718df87a96135eb956401504e555421dc5a3d
---
+++ client.cpp
@@ -0,0 +1,40 @@
+#include <boost/asio.hpp>
+#include <iostream>
+
+std::string getData(boost::asio::ip::tcp::socket &socket) {
+    boost::asio::streambuf buffer;
+    boost::system::error_code error;
+
+    boost::asio::read_until(socket, buffer, '\n', error);
+    if (error) return error.message();
+
+    std::string data = boost::asio::buffer_cast<const char *>(buffer.data());
+    return data;
+}
+
+// Driver program to send data
+void sendData(boost::asio::ip::tcp::socket &socket, const std::string &message) {
+    write(socket,
+          boost::asio::buffer(message + "\n"));
+}
+
+int main() {
+    boost::asio::io_context io_context_object;
+    boost::asio::ip::tcp::socket socket_object(io_context_object);
+    socket_object.connect(
+            boost::asio::ip::tcp::endpoint(
+                    boost::asio::ip::address::from_string("127.0.0.1"),
+                    8888
+            )
+    );
+
+    while (true) {
+        std::string message;
+        std::cout << "Enter message: ";
+        std::cin >> message;
+        sendData(socket_object, message);
+        std::cout << "Server replied: " << getData(socket_object) << std::endl;
+    }
+
+    return 0;
+}
